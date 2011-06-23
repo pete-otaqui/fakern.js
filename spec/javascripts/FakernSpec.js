b58064129bc23e59c23cc881cd23559068754c07
Pete Otaqui <pete@otaqui.com>
2011-06-23 13:47:22 +0100

    started to add more setup, but actually think fakern.js should be a jquery plugin

--- spec/javascripts/FakernSpec.js
+++ spec/javascripts/FakernSpec.js
@@ -1,7 +1,13 @@
+
 describe("Fakern", function() {
 
   beforeEach(function() {
       
   });
+  
+  it("should kern VA", function() {
+      var test = $('<p class="fakern">VA</p>').appendTo('body');
+      Fakern.kern(test[0]);
+  })
 
 });
--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -1,5 +1,5 @@
+
 beforeEach(function() {
   this.addMatchers({
-      
   })
 });

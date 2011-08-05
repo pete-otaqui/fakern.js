4029753676308e273a79418e76d76bb34b6067a4
Lorenzo Campanis <lorenzo.campanis@mangahigh.com>
2011-08-05 18:41:16 +0000

    Refactoring based on Adobe values, added include exclude options

--- jquery.fakern.js
+++ jquery.fakern.js
@@ -1,75 +1,351 @@
-
-
-
+/*
+ *   The Fakern plugin will apply kerning values on specific letter combinations.
+ *   The values for the pairs were taken from a sample list from the adobe site
+ *   http://partners.adobe.com/public/developer/opentype/index_kerning1.html
+ *   The plugin will respect the element's font size regardless of its unit. Hence the kerning itself is unitless.
+ *   The project is sponsored by Mangahigh.com
+ *   
+ *   @example
+ *   $('body').fakern({ 
+ *       include : { 
+ *           '@' : [ ['a',-10], ['b',-20] ],
+ *           '$' : [ ['a',-10], ['b',-20] ] 
+ *       },
+ *       
+ *       exclude : {
+ *           a : true,
+ *           B : ['a','b','C'] 
+ *       }
+ *   });
+ *  
+ *   @author <a href="http://lcampanis.com" target="_blank">Lorenzo Campanis</a>
+ *   @author <a href="http://otaqui.com" target="_blank">Pete Otaqui</a>, 
+ */
 (function($) {
-    var fonts = {
-        // [ [TopLeft, Left, BottomLeft] , [TopRight, Right, BottomRight] ] 
-        arial: {
-            'a': ['T', 'V', 'W', 'Y'],
-            'b': ['T', 'V', 'W', 'Y'],
-            'c': ['T', 'V', 'W', 'Y'],
-            'd': ['T', 'V', 'W', 'Y'],
-            'e': ['T', 'V', 'W', 'Y'],
-            'f': ['T', 'V', 'W', 'Y'],
-            'g': ['T', 'V', 'W', 'Y'],
-            'h': ['T', 'V', 'W', 'Y'],
-            'i': ['T', 'V', 'W', 'Y'],
-            'j': ['T', 'V', 'W', 'Y'],
-            'k': ['T', 'V', 'W', 'Y'],
-            'l': ['T', 'V', 'W', 'Y'],
-            'm': ['T', 'V', 'W', 'Y'],
-            'n': ['T', 'V', 'W', 'Y'],
-            'o': ['T', 'V', 'W', 'Y'],
-            'p': ['T', 'V', 'W', 'Y'],
-            'q': ['T', 'V', 'W', 'Y'],
-            'r': ['T', 'V', 'W', 'Y'],
-            's': ['T', 'V', 'W', 'Y'],
-            't': ['T', 'V', 'W', 'Y'],
-            'u': ['T', 'V', 'W', 'Y'],
-            'v': ['A', 'T'],
-            'w': ['A', 'T'],
-            'x': ['T', 'V', 'W', 'Y'],
-            'y': ['T', 'V', 'W', 'Y'],
-            'z': ['T', 'V', 'W', 'Y'],
-            'A': ['T', 'V', 'W', 'Y', 'v'],
-            'T': ['a', 'b', 'c', 'd', 'e', 'g', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z', 'A'],
-            'V': ['a', 'b', 'c', 'd', 'e', 'g', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z', 'A'],
-            'W': ['a', 'b', 'c', 'd', 'e', 'g', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'x', 'y', 'z', 'A'],
-            'Y': ['a', 'b', 'c', 'd', 'e', 'g', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z', 'A']
-        }
-    };
     
-    
-    
+    /*
+     *  @param {Object} [opts] Additional options
+     *  @param {Object} [opts.include] A list of multidimentional arrays with additional letter combinations. 
+     *  @param {Object} [opts.exclude] A list of multidimentional arrays with letter combinations to be excluded or set some letters to true to exclude all letter combinations
+     */
     $.fn.fakern = function(opts) {
-        var font,
-            shouldKern = function(l, r) {
-                if ( !font[l] ) return false;
-                if ( font[l].indexOf(r) === -1 ) return false;
-                return true;
-            };
-        opts = $.extend({}, {font:'arial'}, opts);
-        font = fonts[opts.font];
+        var pairs = {
+            A : [['y', -92],
+                 ['w', -92],
+                 ['v', -74],
+                 ['’', -111],
+                 ['Y', -105],
+                 ['W', -90],
+                 ['V', -135],
+                 ['U', -55],
+                 ['T', -111],
+                 ['Q', -55],
+                 ['O', -55],
+                 ['G', -40],
+                 ['C', -40]],
+
+            B : [['U', -10],
+                 ['A', -35]],
+
+            D : [['Y', -55],
+                 ['W', -30],
+                 ['V', -40],
+                 ['A', -40]],
+
+            F : [['.', -80],
+                 ['o', -15],
+                 [',', -80],
+                 ['a', -15],
+                 ['A', -74]],
+
+            J : [['A', -60]],
+
+            K : [['y', -25],
+                 ['u', -15],
+                 ['o', -35],
+                 ['e', -25],
+                 ['O', -30]],
+
+            L : [['y', -55],
+                 ['’', -92],
+                 ['Y', -100],
+                 ['W', -74],
+                 ['V', -100],
+                 ['T', -92]],
+
+            N : [['A', -35]],
+
+            O : [['Y', -50],
+                 ['X', -40],
+                 ['W', -35],
+                 ['V', -50],
+                 ['T', -40],
+                 ['A', -35]],
+
+            P : [['.', -111],
+                 [',', -111],
+                 ['a', -15],
+                 ['A', -92]],
+
+            Q : [['U', -10]],
+
+            R : [['Y', -65],
+                 ['W', -55],
+                 ['V', -80],
+                 ['U', -40],
+                 ['T', -60],
+                 ['O', -40]],
+
+            T : [['y', -80],
+                 ['w', -80],
+                 ['u', -45],
+                 [';', -55],
+                 ['r', -35],
+                 ['.', -74],
+                 ['o', -80],
+                 ['i', -35],
+                 ['‐', -92],
+                 ['e', -70],
+                 [',', -74],
+                 [':', -50],
+                 ['a', -80],
+                 ['O', -18],
+                 ['A', -93]],
+
+            U : [['A', -40]],
+
+            V : [['u', -75],
+                 [';', -74],
+                 ['.', -129],
+                 ['o', -129],
+                 ['i', -60],
+                 ['‐', -100],
+                 ['e', -111],
+                 [',', -129],
+                 [':', -74],
+                 ['a', -111],
+                 ['O', -40],
+                 ['G', -15],
+                 ['A', -135]],
+
+            W : [['y', -73],
+                 ['u', -50],
+                 [';', -37],
+                 ['.', -92],
+                 ['o', -80],
+                 ['i', -40],
+                 ['‐', -65],
+                 ['e', -80],
+                 [',', -92],
+                 [':', -37],
+                 ['a', -80],
+                 ['O', -10],
+                 ['A', -120]],
+
+            Y : [['u', -111],
+                 [';', -92],
+                 ['.', -129],
+                 ['o', -110],
+                 ['i', -55],
+                 ['‐', -111],
+                 ['e', -100],
+                 [',', -129],
+                 [':', -92],
+                 ['a', -100],
+                 ['O', -30],
+                 ['A', -120]],
+
+            a : [['w', -15],
+                 ['v', -20]],
+
+            b : [['v', -15],
+                 ['u', -20],
+                 ['.', -40]],
+
+            c : [['y', -15]],
+
+            ',': [['’', -70]
+                  ['”', -70]],
+
+            e : [['y', -15],
+                 ['x', -15],
+                 ['w', -25],
+                 ['v', -25],
+                 ['g', -15]],
+
+            f : [['’', 55],
+                 ['i', -20],
+                 ['f', -25],
+                 ['ı', -50],
+                 ['a', -10]],
+
+            g : [['a', -5]],
+
+            h : [['y', -5]],
+
+            i : [['v', -25]],
+
+            k : [['y', -15],
+                 ['o', -10],
+                 ['e', -10]],
+
+            l : [['w', -10]],
+
+            n : [['y', -15],
+                 ['v', -40]],
+
+            o : [['y', -10],
+                 ['w', -25],
+                 ['v', -15]],
+
+            p : [['y', -10]],
+
+            '.':[['’', -70],
+                 ['”', -70]],
+
+            '“':[['A', -80]],
+
+
+            '‘':[['‘', -74],
+                 ['A', -80]],
+
+            '’':[['v', -50],
+                 ['t', -18],
+                 [' ', -74],
+                 ['s', -55],
+                 ['r', -50],
+                 ['’', -74],
+                 ['l', -10],
+                 ['d', -50]],
+
+            r : [['.', -55],
+                 ['‐', -20],
+                 ['g', -18],
+                 [',', -40]],
+
+
+            ' ':[['Y', -90],
+                 ['W', -30],
+                 ['V', -50],
+                 ['T', -18],
+                 ['A', -55]],
+
+            v : [['.', -65],
+                 ['o', -20],
+                 ['e', -15],
+                 [',', -65],
+                 ['a', -25]],
+
+            w : [['.', -65],
+                 ['o', -10],
+                 [',', -65],
+                 ['a', -10]],
+
+            x : [['e', -15]],
+
+            y : [['.', -65],
+                 [',', -65]]
+        },
+
+        /*
+            Searches for a value inside a multiple array
+            @function 
+            @private
+            @param {number|string} val The needle
+            @param {array} ar The haystack
+            @param {boolean} [strict=false] Strict type comparison
+            @returns {boolean|array} Boolean if value is not present | The last array in the hierarchy that the value was present in
+        */
+        _inMultiArray = function(val, ar, strict) {
+            if(!ar) {
+                return false;
+            }
+
+            var ret = [];
+            strict = strict || false;
+            
+            for(var i=0; i<ar.length; ++i) {                
+                if( (strict && ar[i] === val) ||
+                    (!strict && ar[i] == val)) {
+                    return ar;
+                }
+                else {
+                    if($.isArray( ar[i] )) {
+                        if(ret = inMultiArray(val, ar[i], strict)) {
+                            return ret;
+                        }
+                    }
+                    else {
+                        return false;               
+                    }
+                }
+            }            
+        },
+
+        /*
+            Checks if a ltter should be excluded
+        */
+        _isExcluded = function(letter, next) {
+            var obj = opts.exclude,
+                next = next || -1;
+
+            if(obj.hasOwnProperty(letter)) {
+                if(obj[letter] === true || $.inArray(next, obj[letter]) !== -1) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+               
+        /*
+            Checks if two letters should be kerned
+            @function 
+            @private
+            @param l {string} The letter on the left, which should be kerned
+            @param r {string} The letter on the right. If present, the letter on the left will be kerned
+            @returns {boolean|array} Boolean if the letter should not be kerned | The array with the letter/value combination
+        */
+        _shouldKern = function(l, r) {
+            var ret = inMultiArray(r, pairs[l]);
+            
+            console.log(ret,l,r)
+            if ( !pairs[l] || isExcluded(l, r) || ret === false )  {
+                return false;
+            }
+
+            return ret;
+        };
+
+        opts = $.extend({
+            exclude : {},
+            include : {}
+        }, opts);        
+
+        pairs = $.extend(pairs, opts.include);
+
         return this.each(function() {
             var $ele = $(this),
                 text = $ele.text(),
                 ltrs = text.split(''),
                 strn = '',
-                len = ltrs.length;
-                $(ltrs).each(function(idx, ltr) {
-                    var cur = ltr,
-                        nxt = ltrs[idx+1],
-                        krnLeft, krnRight, cTop, cMid, cBot;
-                    console.group(ltr+', '+nxt);
-                    if ( idx < len-1 ) {
-                        if ( shouldKern(ltr, nxt) ) {
-                            cur = '<span class="fakern">'+cur+'</span>';
-                        }
+                len = ltrs.length,
+                fontSize = parseInt($ele.css('fontSize'), 10);
+            
+            $(ltrs).each(function(idx, ltr) {
+                var cur = ltr,
+                    nxt = ltrs[idx+1],
+                    kern;
+
+                if ( idx < len-1) {                    
+                    if (kern = shouldKern(ltr, nxt)) {
+                        cur = '<span class="fakern" style="margin-right:'+((kern[1])/1000*fontSize)+'">'+cur+'</span>';
                     }
-                    console.groupEnd(ltr+', '+nxt);
-                    strn += cur;
-                });
-                $ele.html(strn);
+                }
+
+                strn += cur;
+            });
+            $ele.html(strn);
         });
     };  
     
--- spec/javascripts/FakernSpec.js
+++ spec/javascripts/FakernSpec.js
@@ -7,7 +7,7 @@ describe("Fakern", function() {
   
   it("should kern AVTaT", function() {
       var test,
-          html = '<span class="fakern">A</span>V<span class="fakern">T</span><span class="fakern">a</span>T';
+          html = '<span class="fakern" style="margin-right:-6.48">A</span>V<span class="fakern" style="margin-right:-3.84">T</span>aT';
       $('<p class="UNFAKED">AVTaT</p>').appendTo('body');
       test = $('<p class="FAKED">AVTaT</p>').appendTo('body').fakern();
       expect(test.html()).toEqual(html);
@@ -26,6 +26,12 @@ describe("Fakern", function() {
       });
       $('<p class="FAKED">'+str+'</p>').appendTo('body').fakern();
       $('<p class="UNFAKED">'+str+'</p>').appendTo('body');
+  });
+
+  it("should kern italic", function() {
+      var $ele = $('<p style="font-style:italic;">one, two</p>').appendTo('body');
+      $ele.fakern();
+      expect($ele.html()).toEqual('one, t<span class="fakern" style="margin-right:-0.48">w</span>o');
   });
 
 });
